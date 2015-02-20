524785d3e958c4fd5c5301d6921926a032756e6a
XStargate <xsong8486@gmail.com>
2015-02-20 18:25:50 -0500

    Add constants, default values, and constraints for npar
    Constants: include const which includes constants required in the pycmt3d
    
    Default values: set default values for dlocation, ddepth, dmoment, ddelta
    
    Constraints for npar: the npar has to be 6, 7, 9, 10, or 11

--- src/pycmt3d/config.py
+++ src/pycmt3d/config.py
@@ -5,14 +5,26 @@
 Configuration object for pycmt3d
 """
 import numpy as np
+import const
 
 class Config(object):
 
-    def __init__(self, npar, dlocation, ddepth, dmoment, ddelta,
+    def __init__(self, npar, dlocation=const.SCALE_LOCATION, ddepth=const.SCALE_DEPTH,
+                 dmoment=const.SCALE_MOMENT, ddelta=const.SCALE_DELTA,
                  weight_data=True, weight_function=None,
                  station_correction=True, zero_trace=True,
                  double_couple=True, lamda_damping=0.0):
         self.npar = npar
+        if (not self.npar in [6, 7, 9, 10, 11]):
+            print '''Error: the current npar (number of parameters) is ''', self.npar,'''
+            The npar (number of parameters) must be 6, 7, 9, 10 or 11"
+            When npar is 6: moment tensor
+            When npar is 7: moment tensor + depth
+            When npar is 9: moment tensor + depth + location(e.g. latitude and longitude)
+            When npar is 10: moment tensor + depth + location + time
+            When npar is 11: moment tensor + depth + location + time + half duration
+            '''
+            return None
         self.dlocation = dlocation
         self.ddepth = ddepth
         self.dmomoent = dmoment
@@ -26,8 +38,10 @@ class Config(object):
 
         self.par_name = ("Mrr", "Mtt", "Mpp", "Mrt", "Mrp", "Mtp",
                          "dep", "lon", "lat", "ctm", "hdr")
-        self.scale_par = np.array([SCALE_MOMENT, SCALE_MOMENT, SCALE_MOMENT, SCALE_MOMENT
-                          SCALE_MOMENT, SCALE_MOMENT, SCALE_DEPTH, SCALE_DDELTA,
-                          SCALE_DDELTA, SCALE_CTIME, SCALE_HDR])
-        self.dcmt_par = np.array([dmoment, dmoment, dmoment, dmoment, dmoment,
-                            dmoment, ddepth, ddelta, ddelta, 1.0, 1.0])/self.scale_par
+        self.scale_par = np.array([const.SCALE_MOMENT, const.SCALE_MOMENT, const.SCALE_MOMENT,
+                                   const.SCALE_MOMENT, const.SCALE_MOMENT, const.SCALE_MOMENT,
+                                   const.SCALE_DEPTH, const.SCALE_DELTA, const.SCALE_DELTA,
+                                   const.SCALE_CTIME, const.SCALE_HDUR])
+        self.dcmt_par = np.array([self.dmoment, self.dmoment, self.dmoment, self.dmoment,
+                                  self.dmoment, self.dmoment, self.ddepth, self.ddelta,
+                                  self.ddelta, 1.0, 1.0])/self.scale_par
