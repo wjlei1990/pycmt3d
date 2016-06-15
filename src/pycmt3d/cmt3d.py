fix issues in envelope calculation

src/pycmt3d/cmt3d.py
@@ -13,7 +13,7 @@ from .measure import compute_derivatives, calculate_variance_on_trace
 from .measure import compute_new_syn_on_trwin
 from .plot_util import plot_seismograms, PlotInvSummary, PlotStats
 from .data_container import MetaInfo
-from .weight import Weight
+from .weight import Weight, setup_energy_weight
 from .constant import NPARMAX
 from .solver import solver
 from .log_util import print_inversion_summary
@@ -122,6 +122,8 @@ class Cmt3D(object):
             meta.weights = weight_meta.weights
             meta.prov.update(weight_meta.prov)
 
+        setup_energy_weight(self.metas, self.data_container)
+
     def _init_metas(self):
         """
         Initialize the self.metas list. Keep the same order with the
@@ -174,6 +176,54 @@ class Cmt3D(object):
 
         return generate_newcmtsource(self.cmtsource, new_cmt_par)
 
+    def _ensemble_measurements(self):
+        """ ensemble measurements from each window """
+        npar = self.config.npar
+        Aw_all = np.zeros([npar, npar])
+        bw_all = np.zeros(npar)
+        Ae_all = np.zeros([npar, npar])
+        be_all = np.zeros(npar)
+        # ensemble matrix and measurement
+        total_wav_energy = 0
+        total_env_energy = 0
+        for _meta in self.metas:
+            if self.config.weight_config.normalize_by_energy:
+                wav_weight = _meta.weights / _meta.prov["wav_energy"]
+                env_weight = _meta.weights / _meta.prov["env_energy"]
+                Aw = sum_matrix(_meta.Aws, coef=wav_weight)
+                bw = sum_matrix(_meta.bws, coef=wav_weight)
+                Ae = sum_matrix(_meta.Aes, coef=env_weight)
+                be = sum_matrix(_meta.bes, coef=env_weight)
+            else:
+                Aw = sum_matrix(_meta.Aws, coef=_meta.weights)
+                bw = sum_matrix(_meta.bws, coef=_meta.weights)
+                Ae = sum_matrix(_meta.Aes, coef=_meta.weights)
+                be = sum_matrix(_meta.bes, coef=_meta.weights)
+            total_wav_energy += sum(_meta.prov["wav_energy"])
+            total_env_energy += sum(_meta.prov["env_energy"])
+
+            Aw_all += Aw
+            bw_all += bw
+            Ae_all += Ae
+            be_all += be
+
+        # ratio between waveform energy and envelope energy
+        cat_ratio = total_wav_energy / total_env_energy
+        logger.debug("total waveform and envelope energy, ratio: %e, %e, %f"
+                     % (total_wav_energy, total_env_energy, cat_ratio))
+
+        ecoef = self.config.envelope_coef
+        if self.config.weight_config.normalize_by_energy:
+            A_all = (1 - ecoef) * Aw_all + ecoef * Ae_all
+            b_all = (1 - ecoef) * bw_all + ecoef * be_all
+        else:
+            A_all = (1 - ecoef) * Aw_all + \
+                ecoef * Ae_all * cat_ratio
+            b_all = (1 - ecoef) * bw_all + \
+                ecoef * be_all * cat_ratio
+
+        return Aw_all, bw_all, Ae_all, be_all, A_all, b_all
+
     def invert_cmt(self):
         """
         ensemble all measurements together to form Matrix A and vector
@@ -186,43 +236,27 @@ class Cmt3D(object):
         logger.info("CMT Inversion")
         logger.info("*"*15)
 
-        npar = self.config.npar
-        Aw_all = np.zeros([npar, npar])
-        bw_all = np.zeros(npar)
-        Ae_all = np.zeros([npar, npar])
-        be_all = np.zeros(npar)
-        # ensemble A and b
-        for _meta in self.metas:
-            Aw = sum_matrix(_meta.Aws, coef=_meta.weights)
-            bw = sum_matrix(_meta.bws, coef=_meta.weights)
-            Ae = sum_matrix(_meta.Aes, coef=_meta.weights)
-            be = sum_matrix(_meta.bes, coef=_meta.weights)
-            Aw_all += Aw
-            bw_all += bw
-            Ae_all += Ae
-            be_all += be
+        Aw_all, bw_all, Ae_all, be_all, A_all, b_all = \
+            self._ensemble_measurements()
 
-        ecoef = self.config.envelope_coef
-        A_all = (1 - ecoef) * Aw_all + ecoef * Ae_all
-        b_all = (1 - ecoef) * bw_all + ecoef * be_all
-
-        logger.info("Inversion Matrix A(with scaled cmt perturbation) is "
-                    "as follows:")
-        logger.info("\n%s" % ('\n'.join(map(_float_array_to_str, A_all))))
         logger.info("Inversion Matrix Aw(with scaled cmt perturbation) is "
                     "as follows:")
         logger.info("\n%s" % ('\n'.join(map(_float_array_to_str, Aw_all))))
+        logger.info("bw_all: [%s]" % (_float_array_to_str(bw_all)))
         logger.info("Inversion Matrix Ae(with scaled cmt perturbation) is "
                     "as follows:")
         logger.info("\n%s" % ('\n'.join(map(_float_array_to_str, Ae_all))))
+        logger.info("be_all: [%s]" % (_float_array_to_str(be_all)))
+        logger.info("Inversion Matrix A(with scaled cmt perturbation) is "
+                    "as follows:")
+        logger.info("\n%s" % ('\n'.join(map(_float_array_to_str, A_all))))
+        logger.info("b_all: [%s]" % (_float_array_to_str(b_all)))
         logger.info("Condition number of A: %10.2f" % (np.linalg.cond(A_all)))
         logger.info("RHS vector b(with scaled cmt perturbation) is "
                     "as follows:")
 
-        logger.info("b_all")
-        logger.info(b_all)
-        logger.info("[%s]" % (_float_array_to_str(b_all)))
-
+        ecoef = self.config.envelope_coef
+        logger.info("waveform and envelope coef: %f, %f" % (1 - ecoef, ecoef))
         # source inversion
         logger.info("-" * 10 + " inversion " + "-" * 10)
         self.new_cmtsource = self.invert_solver(
@@ -438,7 +472,7 @@ class Cmt3D(object):
                 "no_normener"
         else:
             prefix = "%dp_%s.%s" % (
-                self.config.npar, constr_str, self.config.norm_mode)
+                self.config.npar, constr_str, "normener")
 
         if not self.config.weight_config.normalize_by_category:
             prefix += ".no_normcat"

src/pycmt3d/measure.py
@@ -186,7 +186,7 @@ def get_f_df(npar, A, b, m, lam, mstart, fij, f0):
     fij[npar + 1, 0:NM] = dc2_dm
 
 
-def calculate_dsyn(datalist, win_idx, parlist, dcmt_par, taper=None):
+def calculate_dsyn(datalist, win_idx, parlist, dcmt_par, taper_type):
     """
     Calculate dsyn matrix based on perturbed seismograms. Only synt
     and perturbed synthetic are used here:
@@ -198,6 +198,7 @@ def calculate_dsyn(datalist, win_idx, parlist, dcmt_par, taper=None):
     istart = win_idx[0]
     iend = win_idx[1]
     win_len = iend - istart
+    taper = construct_taper(win_len, taper_type=taper_type)
 
     dsyn = np.zeros((len(parlist), win_len))
     for itype, type_name in enumerate(parlist):
@@ -231,7 +232,7 @@ def calculate_dsyn(datalist, win_idx, parlist, dcmt_par, taper=None):
     return dsyn
 
 
-def calculate_denv(datalist, win_idx, parlist, dcmt_par, taper=None):
+def calculate_denv(datalist, win_idx, parlist, dcmt_par, taper_type):
     """
     Calculate dsyn matrix based on perturbed seismograms. Only synt
     and perturbed synthetic are used here:
@@ -244,6 +245,7 @@ def calculate_denv(datalist, win_idx, parlist, dcmt_par, taper=None):
     istart = win_idx[0]
     iend = win_idx[1]
     win_len = iend - istart
+    taper = construct_taper(win_len, taper_type=taper_type)
 
     denv = np.zeros((len(parlist), win_len))
     for itype, type_name in enumerate(parlist):
@@ -293,47 +295,68 @@ def compute_derivatives(datalist, win_time, parlist, dcmt_par,
     check_trace_consistent(obsd, synt)
     win_idx = get_window_idx(win_time, obsd.stats.delta)
 
-    win_len = win_idx[1] - win_idx[0]
-    taper = construct_taper(win_len, taper_type=taper_type)
+    denv = calculate_denv(datalist, win_idx, parlist, dcmt_par,
+                          taper_type)
+    Ae, be = compute_envelope_matrix(denv, obsd.data, synt.data, dt,
+                                     win_idx, taper_type)
 
     dsyn = calculate_dsyn(datalist, win_idx, parlist, dcmt_par,
-                          taper=taper)
-    denv = calculate_denv(datalist, win_idx, parlist, dcmt_par,
-                          taper=taper)
-
-    Ae1, be1 = compute_envelope_matrix(denv, obsd.data, synt.data, dt,
-                                       win_idx, taper)
-
-    Ae, be, _ = compute_envelope_derivatives(dsyn, obsd.data, synt.data, dt,
-                                             win_idx, taper)
-    print("Ae1")
-    print("%s" % ('\n'.join(map(_float_array_to_str, Ae1))))
-    print("Ae")
-    print("%s" % ('\n'.join(map(_float_array_to_str, Ae))))
-    Aw, bw = compute_waveform_derivatives(dsyn, obsd.data, synt.data, dt,
-                                          win_idx, taper)
-    return Aw, bw, Ae1, be1
+                          taper_type)
+    Aw, bw = compute_waveform_matrix(dsyn, obsd.data, synt.data, dt,
+                                     win_idx, taper_type)
+    # print("Ae")
+    # print("%s" % ('\n'.join(map(_float_array_to_str, Ae))))
+    return Aw, bw, Ae, be
 
 
-def compute_envelope_matrix(denv, obsd, synt, dt, win_idx, taper):
+def compute_envelope_matrix(denv, obsd, synt, dt, win_idx, taper_type):
     istart = win_idx[0]
     iend = win_idx[1]
 
     obs_array = obsd.copy()
     syn_array = synt.copy()
 
+    istart_d, iend_d, istart_s, iend_s, _, _ = \
+        correct_window_index(obs_array, syn_array, win_idx[0], win_idx[1])
+
+    taper = construct_taper(iend_s - istart_s, taper_type=taper_type)
+
     A1 = np.dot(denv, denv.transpose()) * dt
     b1 = np.sum(
-        (np.abs(hilbert(taper * obs_array[istart:iend])) -
-         np.abs(hilbert(taper * syn_array[istart:iend]))) *
+        (np.abs(hilbert(taper * obs_array[istart_d:iend_d])) -
+         np.abs(hilbert(taper * syn_array[istart_s:iend_s]))) *
         denv * dt, axis=1)
     return A1, b1
 
 
-def compute_envelope_derivatives(dsyn, obsd, synt, dt, win_idx, taper):
+def compute_waveform_matrix(dsyn, obsd, synt, dt, win_idx, taper_type):
     """
-    Compute envelope measurements matrix H and misfit vector G,
+    Compute waveform measurement matrix H and misfit vector G,
     as stated in Appendix in Qinya's paper
+    """
+    # station correction
+    obs_array = obsd.copy()
+    syn_array = synt.copy()
+
+    istart_d, iend_d, istart_s, iend_s, _, _ = \
+        correct_window_index(obs_array, syn_array, win_idx[0], win_idx[1])
+
+    taper = construct_taper(iend_s - istart_s, taper_type=taper_type)
+
+    A1 = np.dot(dsyn, dsyn.transpose()) * dt
+    b1 = np.sum(
+        taper * (obs_array[istart_d:iend_d] -
+                 syn_array[istart_s:iend_s]) *
+        dsyn * dt, axis=1)
+
+    return A1, b1
+
+
+def compute_envelope_matrix_theo(dsyn, obsd, synt, dt, win_idx, taper):
+    """
+    Compute envelope measurements matrix H and misfit vector G theoretically
+    as stated in Appendix in Qinya's paper.
+    Attension: not used!!! BUG inside!!!
     """
     istart = win_idx[0]
     iend = win_idx[1]
@@ -354,27 +377,6 @@ def compute_envelope_derivatives(dsyn, obsd, synt, dt, win_idx, taper):
         (np.abs(hilbert(taper * obs_array[istart:iend])) -
          np.abs(hilbert(taper * syn_array[istart:iend]))) *
         env_derivss * dt, axis=1)
-    return A1, b1, env_derivss
-
-
-def compute_waveform_derivatives(dsyn, obsd, synt, dt, win_idx, taper):
-    """
-    Compute waveform measurement matrix H and misfit vector G,
-    as stated in Appendix in Qinya's paper
-    """
-    # station correction
-    obs_array = obsd.copy()
-    syn_array = synt.copy()
-
-    istart_d, iend_d, istart_s, iend_s, _, _ = \
-        correct_window_index(obs_array, syn_array, win_idx[0], win_idx[1])
-
-    A1 = np.dot(dsyn, dsyn.transpose()) * dt
-    b1 = np.sum(
-        taper * (obs_array[istart_d:iend_d] -
-                 syn_array[istart_s:iend_s]) *
-        dsyn * dt, axis=1)
-
     return A1, b1
 
 
@@ -413,7 +415,7 @@ def calculate_variance_on_trace(obsd, synt, win_time, taper_type="tukey"):
         nshift, cc, dlnA, cc_amp_value = \
             measure_window(obsd, synt, istart, iend)
 
-        taper = construct_taper(iend - istart, taper_type=taper_type)
+        taper = construct_taper(iend_d - istart_d, taper_type=taper_type)
 
         v1_array[_win_idx] = dt * _diff_energy_(obsd.data[istart_d:iend_d],
                                                 synt.data[istart_s:iend_s],

src/pycmt3d/weight.py
@@ -6,7 +6,7 @@ import numpy as np
 from spaceweight import SphereAziBin, Point
 from . import logger
 from .util import distance, get_window_idx
-from .measure import _energy_
+from .measure import _energy_, _envelope
 from .data_container import MetaInfo
 
 
@@ -28,15 +28,30 @@ def calculate_energy_weighting(trwin, mode="window"):
     obsd = trwin.datalist["obsd"]
     dt = obsd.stats.delta
     win_idx = get_window_idx(trwin.windows, dt)
-    energy = np.zeros(trwin.nwindows)
+    wav_energy = np.zeros(trwin.nwindows)
+    env_energy = np.zeros(trwin.nwindows)
+
     if mode == "all":
         average_energy = _energy_(obsd.data) / obsd.stats.npts
-        energy = \
+        wav_energy = \
+            average_energy * (win_idx[:, 1] - win_idx[:, 0]) * dt
+        average_energy = _energy_(_envelope(obsd.data)) / obsd.stats.npts
+        env_energy = \
             average_energy * (win_idx[:, 1] - win_idx[:, 0]) * dt
     elif mode == "window":
         for idx, _win in enumerate(win_idx):
-            energy[idx] = _energy_(obsd.data[_win[0]:_win[1]]) * dt
-    return energy
+            wav_energy[idx] = \
+                _energy_(obsd.data[_win[0]:_win[1]]) * dt
+            env_energy[idx] = \
+                _energy_(_envelope(obsd.data[_win[0]:_win[1]])) * dt
+    return wav_energy, env_energy
+
+
+def setup_energy_weight(metas, data_container):
+    for meta, trwin in zip(metas, data_container):
+        we, ee = calculate_energy_weighting(trwin, mode="all")
+        meta.prov['wav_energy'] = we
+        meta.prov['env_energy'] = ee
 
 
 class Weight(object):
@@ -91,9 +106,6 @@ class Weight(object):
             # components(Z, R, T)
             self.setup_weight_for_component()
 
-        if self.config.normalize_by_energy:
-            self.normalize_weight_by_energy()
-
         self.normalize_weight()
 
         logger.debug("Detailed Weighting information")
@@ -111,12 +123,6 @@ class Weight(object):
         factor = self.data_container.nwindows / weight_sum
         for meta in self.metas:
             meta.weights *= factor
-
-    def normalize_weight_by_energy(self):
-        for meta, trwin in zip(self.metas, self.data_container):
-            energy = calculate_energy_weighting(trwin, mode="all")
-            meta.weights /= energy
-            meta.prov['energy_factor'] = energy
 
     def sort_into_category(self):
         """