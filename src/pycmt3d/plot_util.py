98b5d44a665919f9107fe769deea63163fd4578a
fmagnoni <federica.magnoni@gmail.com>
2016-07-25 14:34:04 +0200

    Update plot_util.py

--- src/pycmt3d/plot_util.py
+++ src/pycmt3d/plot_util.py
@@ -505,11 +505,11 @@ class PlotInvSummary(object):
             ax.set_xlim(minlon-padding, maxlon+padding)
             ax.set_ylim(minlat-padding, maxlat+padding)
             width_beach = min((maxlon+2*padding-minlon)/(4*padding),
-                            (maxlat+2*padding-minlat)/(4*padding))
+                              (maxlat+2*padding-minlat)/(4*padding))
         else:
             width_beach = 20
         bb = beach(focmecs, xy=(cmt_lon, cmt_lat), 
-                   width=width_beach, linewidth=1,alpha=1.0)
+                   width=width_beach, linewidth=1, alpha=1.0)
         bb.set_zorder(10)
         ax.add_collection(bb)
 
